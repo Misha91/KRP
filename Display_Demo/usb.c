30f3f1c89c02d8c677206a72ca072b0c45b35906
Mikhail Ivanov <mikhail.ivanov@rocketmail.com>
2020-01-23 23:27:12 +0100

    some progress

--- Display_Demo/usb.c
+++ Display_Demo/usb.c
@@ -1,6 +1,7 @@
 #include "usb.h"
 #include "lcd.h"
 
+#define REPORT_LENGHT 52
 
 __IO struct struct_usb *usb = (struct struct_usb *) USB_BASE_ADDRESS;
 //usb->reg1 = 0x00;
@@ -10,17 +11,60 @@ const  uint8_t  deviceDescriptor  [ 18 ]  =
 	18 , 			// bLength 
 	1 , 			// bDescriptorType 
 	0x00 ,  0x02 , 	        // bcdUSB 
-	0xFF , 		        // bDeviceClass 
-	0xFF , 		        // bDeviceSubClass 
-	0xFF , 		        // bDeviceProtocol 
+	0x00 , 		        // bDeviceClass 
+	0x00 , 		        // bDeviceSubClass 
+	0x00 , 		        // bDeviceProtocol 
 	64 , 			// bMaxPacketSize0 
 	0xAD ,  0xDE ,          // idVendor 
 	0xEF ,  0xBE ,          // idProduct 
-	0x00 ,  0x01 ,          // bcdDevice
+	0x00 ,  0x02 ,          // bcdDevice
 	0 , 			// iManufacturer 
 	0 , 			// iProduct 
 	0 , 			// iSerialNumber 
 	1 			// bNumConfigurations 
+};
+
+const  uint8_t  configDescriptor  []  =  
+{ 
+    // Configuration descriptor
+    0x09, // bLength: Configuration Descriptor size
+    0x02, // bDescriptorType: Configuration
+    0x22, // wTotalLength: 34 Bytes returned
+    0x00,
+    0x01, // bNumInterfaces: 1 interface
+    0x01, // bConfigurationValue: Configuration value
+    0x00, // iConfiguration: Index of string descriptor describing the configuration
+    0xC0, // bmAttributes: Bus powered and Remote wake up
+    0x0A, // MaxPower 20 mA
+    // Interface descriptor
+    0x09, // bLength: Interface Descriptor size
+    0x04, // bDescriptorType: Interface descriptor type
+    0x01, // bInterfaceNumber: Number of Interface )
+    0x00, // bAlternateSetting: Alternate setting
+    0x01, // bNumEndpoints: one endpoint used
+    0x03, // bInterfaceClass: HID
+    0x01, // bInterfaceSubClass: No subclass
+    0x02, // bInterfaceProtocol: Mouse
+    0x00, // interface: Index of string descriptor
+    // HID descriptor
+    0x09, // bLength: HID Descriptor size
+    0x21, // bDescriptorType: HID
+    0x00, // bcdHID: HID Class Spec release number
+    0x01,
+    0x21, // bCountryCode: Hardware target country US
+    0x01, // bNumDescriptors: Number of HID class descriptors to follow
+    0x22, // bDescriptorType     
+    REPORT_LENGHT, // wItemLength: Report descriptor length low bite
+    0x00,
+    // Endpoint descriptor
+    0x07, // bLength: Endpoint Descriptor size
+    0x05, // bDescriptorType: Endpoint descriptor type
+    0x81, // bEndpointAddress: Endpoint Address (IN)
+    0x03, // bmAttributes: Interrupt endpoint
+    0x08, // wMaxPacketSize: 8 Byte max
+    0x00,
+    0x0A // bInterval: Polling Interval (8 ms) 
+     
 };
 
 volatile int tmp = 0;
@@ -186,13 +230,12 @@ uint8_t send_desc(struct setup_packet * stp_pck)
     
   uint8_t  type  =  (( stp_pck->wValue  >>  8 )  &  0xFF ); 
   uint8_t  index  = ( stp_pck->wValue  &  0xFF ); 
- 
+  
+  volatile static uint32_t TxBuffer[64];
   if  (stp_pck->bRequest == 6 && type  ==  1  &&  index  ==  0  &&  stp_pck->wIndex  ==  0)  
   { 
     
-    uint16_t  length  =  stp_pck->wLength > sizeof(deviceDescriptor) ? sizeof(deviceDescriptor) : stp_pck->wLength;
-    uint32_t *TxBuffer = (uint32_t*)malloc(((int)(length/4))*sizeof(uint32_t));
-    
+    uint16_t  length  =  stp_pck->wLength > sizeof(deviceDescriptor) ? sizeof(deviceDescriptor) : stp_pck->wLength;  
     
     for (n = 0; n < (int)(length/4); n++)
     {
@@ -234,6 +277,57 @@ uint8_t send_desc(struct setup_packet * stp_pck)
     //n=sprintf (buffer, "%#08x %#08x %d", TxBuffer[3], TxBuffer[4], (int)(length/4)); 
     //LCD_printLine(4,0, buffer, n);
   }
+  
+  else if  (stp_pck->bRequest == 6 && type  ==  2  &&  index  ==  0  &&  stp_pck->wIndex  ==  0)  
+  { 
+    if (stp_pck->wLength == 34)
+    {
+      uint8_t bp = 0;
+    }
+    uint16_t  length  =  stp_pck->wLength > sizeof(configDescriptor) ? sizeof(configDescriptor) : stp_pck->wLength;
+    
+    
+    
+    for (n = 0; n < (int)(length/4); n++)
+    {
+      TxBuffer[n] = configDescriptor[n*4] | (configDescriptor[n*4 + 1] << 8) |
+        (configDescriptor[n*4 + 2] << 16) | (configDescriptor[n*4 + 3] << 24);
+            
+    }
+    if (length%4 != 0)
+    {
+      TxBuffer[(int)(length/4)] = 0;
+      for (n = 0; n < length%4; n++)
+      {
+        TxBuffer[(int)(length/4)] |= configDescriptor[((int)(length/4))*4+n]<<(8*n);
+      }
+    }
+    
+    int free_tx = (*( (volatile unsigned long *) OTG_FS_DTXFSTS0) & 0xFFFF)*4;
+    if (free_tx > length)
+    {
+      
+      *( (volatile unsigned long *) OTG_FS_DIEPTSIZ0) = (1<<19) | length;
+      
+      volatile unsigned long tmp_reg = *( (volatile unsigned long *) OTG_FS_DIEPCTL0);
+      tmp_reg &= ~(0x3 | (0xF << 22));
+      tmp_reg |= (1<<26);
+      *( (volatile unsigned long *) OTG_FS_DIEPCTL0) = tmp_reg;
+      *( (volatile unsigned long *) OTG_FS_DIEPCTL0) |= (1<<31);
+      
+      for (n = 0; n <= (int)(length/4); n++)
+      {
+        *( (volatile unsigned long *) (USB_BASE_ADDRESS + 0x1000)) = TxBuffer[n];
+      }
+      //tmp++;
+      //n=sprintf (buffer, "Sent: %d, avail: %d", length, free_tx ); 
+      //LCD_printLine(3,0, buffer, n);
+      return 1;
+    }
+    //n=sprintf (buffer, "%#08x %#08x %d", TxBuffer[3], TxBuffer[4], (int)(length/4)); 
+    //LCD_printLine(4,0, buffer, n);
+  }
+  
   else if (stp_pck->bmRequestType == 0 && stp_pck->bRequest == 5)
   {
     //*( (volatile unsigned long *) OTG_FS_DCFG) &= ~(0x7F0);
@@ -251,11 +345,30 @@ uint8_t send_desc(struct setup_packet * stp_pck)
       needToChangeAddr = 1;
       newAddr = stp_pck->wValue;
       
-      *( (volatile unsigned long *) (USB_BASE_ADDRESS + 0x1000));
+      //*( (volatile unsigned long *) (USB_BASE_ADDRESS + 0x1000));
       
       *( (volatile unsigned long *) OTG_FS_DCFG) |= newAddr << 4;
       //n=sprintf (buffer, "%d", newAddr); 
       //LCD_printLine(4,0, buffer, n);
+      
+      return 1;
+    }
+  }
+    
+    else
+    {
+    //*( (volatile unsigned long *) OTG_FS_DCFG) &= ~(0x7F0);
+    //*( (volatile unsigned long *) OTG_FS_DCFG) |= stp_pck->wValue << 4;
+    int free_tx = (*( (volatile unsigned long *) OTG_FS_DTXFSTS0) & 0xFFFF)*4;
+    if (free_tx > 0)
+    {
+      *( (volatile unsigned long *) OTG_FS_DIEPTSIZ0) = (1<<19);
+      volatile unsigned long tmp_reg = *( (volatile unsigned long *) OTG_FS_DIEPCTL0);
+      tmp_reg &= ~(0x3 | (0xF << 22));
+      tmp_reg |= (1<<26);
+      *( (volatile unsigned long *) OTG_FS_DIEPCTL0) = tmp_reg;
+      *( (volatile unsigned long *) OTG_FS_DIEPCTL0) |= (1<<31);
+      
       
       return 1;
     }
@@ -331,10 +444,11 @@ void OTG_FS_IRQHandler(void){
   {
     unsigned long regMskStorage = *( (volatile unsigned long *) OTG_FS_GINTMSK);
     *( (volatile unsigned long *) OTG_FS_GINTMSK) = 0;
-    unsigned long init_grxstsp = *((volatile unsigned long *) OTG_FS_GRXSTSP);
-    unsigned char pcktStatus = (init_grxstsp >> 17) & (0xF) ;
-    unsigned int bcnt = (init_grxstsp >> 4) & (0x7FF ) ;
-    unsigned char dpid = (init_grxstsp >> 15) & (0x3) ;
+    uint32_t init_grxstsp = *((volatile unsigned long *) OTG_FS_GRXSTSP);
+    uint8_t pcktStatus = (init_grxstsp >> 17) & (0xF) ;
+    uint16_t bcnt = (init_grxstsp >> 4) & (0x7FF ) ;
+    uint8_t dpid = (init_grxstsp >> 15) & (0x3) ;
+    uint8_t ep_num = (init_grxstsp) & (0xF) ;
     if ((pcktStatus == 2 || pcktStatus == 3)) // && needToChangeAddr
     //if(1)
     {
@@ -384,7 +498,7 @@ void OTG_FS_IRQHandler(void){
       stp_pck.wValue = (readBuffer[fifo_num-2] >> 16) & 0xFFFF;
       stp_pck.wIndex = readBuffer[fifo_num-1] & 0xFFFF;
       stp_pck.wLength = (readBuffer[fifo_num-1] >> 16) & 0xFFFF;
-      c=sprintf (buffer, "%#02x %#02x %#02x %#04x %#04x %#04x", ((*( (volatile unsigned long *) OTG_FS_DCFG) >> 4) & 0x7F), stp_pck.bmRequestType, stp_pck.bRequest, stp_pck.wValue, stp_pck.wIndex, stp_pck.wLength); 
+      c=sprintf (buffer, "%d %#02x %#02x %#02x %#04x %#04x %#04x", ep_num, ((*( (volatile unsigned long *) OTG_FS_DCFG) >> 4) & 0x7F), stp_pck.bmRequestType, stp_pck.bRequest, stp_pck.wValue, stp_pck.wIndex, stp_pck.wLength); 
       LCD_printLine(3+packetProcessed,0, buffer, c);
       packetProcessed++;
       send_desc(&stp_pck);
