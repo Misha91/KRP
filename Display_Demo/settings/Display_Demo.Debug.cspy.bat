0414b7cfb48f12c52342f55d2bbde511d2591353
Mikhail Ivanov <mikhail.ivanov@rocketmail.com>
2020-01-31 12:25:26 +0100

    blablabla

--- Display_Demo/settings/Display_Demo.Debug.cspy.bat
+++ Display_Demo/settings/Display_Demo.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\Misha\Desktop\Study\KRP\Semestral\Display_Demo\settings\Display_Demo.Debug.general.xcl" --backend -f "C:\Users\Misha\Desktop\Study\KRP\Semestral\Display_Demo\settings\Display_Demo.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\Misha\Desktop\Study\archive\KRP\Semestral\Display_Demo\settings\Display_Demo.Debug.general.xcl" --backend -f "C:\Users\Misha\Desktop\Study\archive\KRP\Semestral\Display_Demo\settings\Display_Demo.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\Misha\Desktop\Study\KRP\Semestral\Display_Demo\settings\Display_Demo.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\Misha\Desktop\Study\KRP\Semestral\Display_Demo\settings\Display_Demo.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\Misha\Desktop\Study\archive\KRP\Semestral\Display_Demo\settings\Display_Demo.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\Misha\Desktop\Study\archive\KRP\Semestral\Display_Demo\settings\Display_Demo.Debug.driver.xcl" 
 
 @echo off 
 :end
